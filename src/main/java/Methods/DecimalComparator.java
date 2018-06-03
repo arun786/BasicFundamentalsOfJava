7162323cd034cbb86d9c13bf1a053b3b9b3921ed
Adwiti789 <adwiti1975@gmail.com>
2018-06-03 10:30:57 -0700

    Decimal Comparator

---
+++ src/main/java/Methods/DecimalComparator.java
@@ -0,0 +1,15 @@
+package Methods;
+
+/**
+ * Created by Adwiti on 6/3/2018.
+ */
+public class DecimalComparator {
+    /**
+     * To compare two numbers to three decimal places
+     */
+    public static boolean areEqualByThreeDecimalPlaces(double num1, double num2) {
+        int iNum1 = (int) (num1 * 1000);
+        int iNum2 = (int) (num2 * 1000);
+        return iNum1 == iNum2 ? true : false;
+    }
+}
