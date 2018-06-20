1f50e2673ac61c86719464aaee9e16c06442dbf9
Adwiti789 <adwiti1975@gmail.com>
2018-06-19 19:34:08 -0700

    switch case

---
+++ src/main/java/DayOfTheWeekChallenge/SwitchStatement.java
@@ -0,0 +1,45 @@
+package DayOfTheWeekChallenge;
+
+import java.util.Scanner;
+
+/**
+ * Created by Adwiti on 6/19/2018.
+ */
+public class SwitchStatement {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a day ");
+        int day = scanner.nextInt();
+        printDayOfTheWeek(day);
+    }
+
+    public static void printDayOfTheWeek(int day) {
+        switch (day) {
+            case 0:
+                System.out.println("Sunday");
+                break;
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("Invalid option");
+                break;
+        }
+    }
+}
